e0c9932f5045122903780b8bcb90e92631f279a0
hridyeshswami <hridyeshswami@gmail.com>
2026-08-08 16:26:52 +0530

    Create squarePattern.java

---
+++ squarePattern.java
@@ -0,0 +1,16 @@
+class squarePattern {
+
+    static void printsquare(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.println("nikhil");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = 3; 
+        printsquare(size);
+    }
+}
